c8357a34ff3756ced6fbfd5b8fd21501ebc17fc0
rooneyish <ilustraterp@gmail.com>
2025-03-14 09:16:47 +0545

    minor fixes

--- src/main/java/com/week2/controller/HomeController.java
+++ src/main/java/com/week2/controller/HomeController.java
@@ -13,30 +13,24 @@ import java.io.IOException;
  */
 @WebServlet(asyncSupported = true, urlPatterns = { "/home", "/" })
 public class HomeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HomeController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Use request.getRequestDispatcher, not response.getRequestDispatcher
+        request.getRequestDispatcher("WEB-INF/pages/home.jsp").forward(request, response);
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
--- src/main/webapp/WEB-INF/pages/login.jsp
+++ src/main/webapp/WEB-INF/pages/login.jsp
@@ -6,7 +6,7 @@
 <meta charset="UTF-8">
 <title>Login to your account</title>
 <link rel="stylesheet" type="text/css"
-	href="../css/login.css" />
+	href="${pageContext.request.contextPath}/css/login.css" />
 
 <link rel="stylesheet" type="text/css"
 	href="${pageContext.request.contextPath}/css/header.css" />
